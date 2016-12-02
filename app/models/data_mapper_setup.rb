e3c959209ccdbd340f2fd8746d848b6847319909
George Eaton <george_eaton@hotmail.co.uk>
2016-12-02 10:17:39 +0000

    added rakefile

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -7,4 +7,3 @@ require 'dm-postgres-adapter'
 #DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,13 +4,9 @@ require 'dm-postgres-adapter'
 
 
 class Tag
-
   include DataMapper::Resource
   property :id, Serial
   property :tag, String
 
   has n, :links, :through => Resource
-
-
-
 end
