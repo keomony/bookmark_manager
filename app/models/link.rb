2dad87583d018c8603c781d906707659687bf09c
George Eaton <george_eaton@hotmail.co.uk>
2016-12-02 10:22:36 +0000

    added procfile

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,8 +9,4 @@ class Link
   property :url, String
 
   has n, :tags, :through => Resource
-
 end
-
-
-# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
